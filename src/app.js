4000e4ff4b6d0e5ef775471b1f92fda323e58895
teste <teste>
2026-01-28 07:23:59 -0300

    feat: Creating new methods

--- src/app.js
+++ src/app.js
@@ -17,26 +17,6 @@ const app = express();
 routes(app);
 
 
-// app.post('/books', (req, res) => {
-//     books.push(req.body);  
-//     res.status(201).send("Book added successfully!");
-// })
-
-// app.get('/books/:id', (req, res) => {
-//     const index = searchBookById(req.params.id);
-
-//     res.status(200).json(books[index]);
-
-// })
-
-// app.put('/books/:id', (req, res) => { 
-//     const index = searchBookById(req.params.id);
-
-//     books[index].title = req.body.title;
-
-//     res.status(200).json(books);
-// })
-
 // app.delete('/books/:id', (req, res) => { 
 //     const index = searchBookById(req.params.id);
 
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,25 +1,73 @@
-import book from "../models/book.js";
+import Book from "../models/book.js";
 
-class BookController{
+class BookController {
 
-static async listBooks(req, res){
-    const booksList = await book.find({});
-    res.status(200).json(booksList);
-}
+    static async listBooks(req, res) {
+        try {
+            const booksList = await Book.find({});
+            res.status(200).json(booksList);
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message} - failed to retrieve books`
+            })
+        }
 
-static async registerBook(req, res){   
-    try{
-        const newBook = await book.create(req.body);
-        res.status(201).json({
-            message: "Book registered successfully",
-            book: newBook
-        });  
-    }catch(error){
-    res.status(500).json({
-        message: `${error.message} - failed to register book`
-    })
-    }   
-}
+    }
+
+    static async listBookById(req, res) {
+        try {
+            const id = req.params.id;
+            const foundBook = await Book.findById(id);
+            res.status(200).json(foundBook);
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message} - failed to retrieve book`
+            })
+        }
+    }
+
+    static async registerBook(req, res) {
+        try {
+            const newBook = await Book.create(req.body);
+            res.status(201).json({
+                message: "Book registered successfully",
+                book: newBook
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message} - failed to register book`
+            })
+        }
+    }
+
+    static async updateBook(req, res) {
+        try {
+            const id = req.params.id;
+            await Book.findByIdAndUpdate(id, req.body);
+            res.status(200).json({
+                message: "Book updated successfully"
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message} - failed to update book`
+            })
+        }
+    }
+
+    static async deleteBook(req, res) {
+        try {
+            const id = req.params.id;
+            await Book.findByIdAndDelete(id);
+            res.status(200).json({
+                message: "Book deleted successfully"
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: `${error.message} - failed to delete book`
+            })
+        }
+    }
+
 }
 
 
